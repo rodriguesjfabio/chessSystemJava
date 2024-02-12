c98aaa83a43725ea92f33242fc87aee65e0a746d
Fabio Rodrigues <rodrigues.jfabio@gmail.com>
2024-02-12 17:31:23 -0300

    contagem de movimento das pecas

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,6 +6,7 @@ import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
     private Color color;
+    private int moveCount;
 
     public ChessPiece(Board board, Color color) {
         super(board);
@@ -23,6 +24,18 @@ public abstract class ChessPiece extends Piece {
 
     public ChessPosition getChessPosition(){
         return ChessPosition.fromPosition(position);
+    }
+
+    public int getMoveCount(){
+        return moveCount;
+    }
+
+    public void increaseMoveCount(){
+        moveCount++;
+    }
+
+    public void decreaseMoveCount(){
+        moveCount--;
     }
 
 }
